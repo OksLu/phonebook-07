abfbcb07f8738c0ecfde661af7d98c30e3fc04b0
OksLu <112413121+OksLu@users.noreply.github.com>
2023-05-10 17:01:02 +0300

    added createSelector func for filtered contacts

--- src/components/contactsList/ContactsList.jsx
+++ src/components/contactsList/ContactsList.jsx
@@ -1,6 +1,6 @@
 import { Contact } from 'components/contactEl/Contact';
 import {
-  getContacts,
+  filteredContacts,
   getError,
   getFilter,
   getIsLoading,
@@ -13,14 +13,9 @@ import Loader from 'components/Loader/Loader';
 
 export const ContactsList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
+  const contacts = useSelector(filteredContacts);
   const loading = useSelector(getIsLoading);
   const error = useSelector(getError);
-
-  const filteredContacts = contacts
-    .filter(({ name }) => name.toLowerCase().includes(filter))
-    .sort((a, b) => a.name.localeCompare(b.name));
 
   useEffect(() => {
     dispatch(fetchAllThunk());
@@ -30,7 +25,7 @@ export const ContactsList = () => {
     <ul className={css.list}>
       {loading && <Loader />}
       {error && <div>{error}</div>}
-      {filteredContacts.map(({ id, name, phone }) => {
+      {contacts.map(({ id, name, phone }) => {
         return (
           <li key={id} className={css.item}>
             <Contact id={id} name={name} phone={phone} />
